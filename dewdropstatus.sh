50f3ac4487146acebb533f1e97a3f52bef539be9
Sophie Young <flwrstems@proton.me>
2025-05-23 13:44:07 -0400

    finish fixing flwrstemsstatus.sh

--- dewdropstatus.sh
+++ dewdropstatus.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-STATUS_FILE="/var/www/gooptech.nya.pub/dewdropstatus.html"
-mkdir -p "$(dirname "$STATUS_FILE")"
+REMOTE_PATH="/var/www/gooptech.nya.pub/dewdropstatus.html"
+mkdir -p "$(dirname "$REMOTE_PATH")"
 
-LOG_FILE="/home/flwrstems/dewdropstatus.log"
-mkdir -p "$(dirname "$LOG_FILE")"
+REMOTE_LOG="/home/flwrstems/dewdropstatus.log"
+mkdir -p "$(dirname "$REMOTE_LOG")"
+
+LOCAL_PATH="/home/flwrstems/dewdropstatus.html"
+mkdir -p "$(dirname "$LOCAL_PATH")"
+
+LOCAL_LOG="/home/flwrstems/dewdropstatus.log"
+mkdir -p "$(dirname "$LOCAL_LOG")"
+
+FRANKIE_USER="flwrstems"
+FRANKIE_HOST="173.88.84.88"
 
 
 { 
@@ -30,4 +39,6 @@ mkdir -p "$(dirname "$LOG_FILE")"
     iostat -xz ld0
     echo -e '\n=== df -h / ==='
     df -h /
-} | aha -b -t 'status' > "$STATUS_FILE" 2> "$LOG_FILE"
+} | aha -b -t 'dewdropstatus' > "$LOCAL_PATH" 2> "$LOCAL_LOG"
+
+scp "$LOCAL_PATH" "$FRANKIE_USER@$FRANKIE_HOST:$REMOTE_PATH"
